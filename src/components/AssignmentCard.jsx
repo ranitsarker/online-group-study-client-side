7dabf310a73fbc31d6627ffec6947df96a304d61
ranitsarker <rntprince@gmail.com>
2023-11-05 20:54:46 +0600

    assignment updated work done

--- src/components/AssignmentCard.jsx
+++ src/components/AssignmentCard.jsx
@@ -1,6 +1,9 @@
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 const AssignmentCard = ({ assignment }) => {
+    const marks = parseInt(assignment.marks);
+
     return (
         <div className="border border-gray-200 p-4 rounded-md hover:shadow-md">
             <img
@@ -9,17 +12,28 @@ const AssignmentCard = ({ assignment }) => {
                 className="w-full h-48 object-cover rounded-md"
             />
             <h3 className="mt-2 text-xl font-semibold">{assignment.title}</h3>
-            <p className="mt-2">Marks: {assignment.marks}</p>
+            <p className="mt-2">Marks: {marks}</p>
             <p>Difficulty: {assignment.difficulty}</p>
             <div className="mt-4 flex space-x-2">
                 <Link to={`/assignment/${assignment._id}`} className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
                     View Assignment
                 </Link>
-                <button className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
+                <Link to={`/update-assignment/${assignment._id}`} className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700">
                     Update Assignment
-                </button>
+                </Link>
+
             </div>
         </div>
     );
 };
+AssignmentCard.propTypes = {
+    assignment: PropTypes.shape({
+        thumbnailUrl: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        marks: PropTypes.number.isRequired,
+        difficulty: PropTypes.string.isRequired,
+        _id: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
 export default AssignmentCard;
--- src/pages/UpdateAssignment.jsx
+++ src/pages/UpdateAssignment.jsx
@@ -1,9 +1,141 @@
-
+import { useContext, useState } from 'react';
+import toast from 'react-hot-toast';
+import { AuthContext } from '../providers/AuthProvider';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const UpdateAssignment = () => {
+    const navigate = useNavigate();
+    const { user } = useContext(AuthContext);
+    const loadedAssignment = useLoaderData();
+    
+    // Initialize form state with default values
+    const [formData, setFormData] = useState({
+        title: loadedAssignment.title || '',
+        description: loadedAssignment.description || '',
+        marks: loadedAssignment.marks || 0,
+        difficulty: loadedAssignment.difficulty || 'easy',
+        thumbnailUrl: loadedAssignment.thumbnailUrl || '',
+    });
+
+    const handleAssignmentUpdate = (e) => {
+        e.preventDefault();
+        const { title, description, marks, difficulty, thumbnailUrl } = formData;
+
+        // You can access `loadedAssignment._id` to identify the assignment to be updated
+        const assignmentId = loadedAssignment._id;
+
+        // Send a request to your server to update the assignment
+        fetch(`http://localhost:5000/update-assignment/${assignmentId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title,
+                description,
+                marks,
+                difficulty,
+                thumbnailUrl,
+                createdBy: user ? user.email : null,
+            }),
+        })
+            .then((response) => response.json())
+            .then(() => {
+                toast.success('Assignment updated successfully');
+                // Redirect to the view assignment page or any other page
+                navigate(`/assignment/${assignmentId}`);
+            })
+            .catch((error) => {
+                toast.error('An error occurred while updating the assignment');
+                console.error(error);
+            });
+    };
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    };
+
     return (
-        <div>
-            UpdateAssignment
+        <div className="max-w-md mx-auto">
+            <h2 className="text-2xl font-semibold mb-4">Update Assignment</h2>
+            <form className="space-y-4" onSubmit={handleAssignmentUpdate}>
+                <div>
+                    <label htmlFor="title" className="block text-sm font-medium text-gray-700">
+                        Title:
+                    </label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        value={formData.title}
+                        onChange={handleChange}
+                        className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="description" className="block text-sm font-medium text-gray-700">
+                        Description:
+                    </label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        value={formData.description}
+                        onChange={handleChange}
+                        className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    ></textarea>
+                </div>
+                <div>
+                    <label htmlFor="marks" className="block text-sm font-medium text-gray-700">
+                        Marks:
+                    </label>
+                    <input
+                        type="number"
+                        id="marks"
+                        name="marks"
+                        value={formData.marks}
+                        onChange={handleChange}
+                        className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="difficulty" className="block text-sm font-medium text-gray-700">
+                        Difficulty:
+                    </label>
+                    <select
+                        id="difficulty"
+                        name="difficulty"
+                        value={formData.difficulty}
+                        onChange={handleChange}
+                        className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    >
+                        <option value="easy">Easy</option>
+                        <option value="medium">Medium</option>
+                        <option value="hard">Hard</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="thumbnailUrl" className="block text-sm font-medium text-gray-700">
+                        Thumbnail Image URL:
+                    </label>
+                    <input
+                        type="url"
+                        id="thumbnailUrl"
+                        name="thumbnailUrl"
+                        value={formData.thumbnailUrl}
+                        onChange={handleChange}
+                        className="mt-1 p-2 block w-full border rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+                    />
+                </div>
+                <button
+                    type="submit"
+                    className="mt-4 bg-indigo-600 text-white font-semibold px-4 py-2 rounded-full hover-bg-indigo-700"
+                >
+                    Update Assignment
+                </button>
+            </form>
         </div>
     );
 };
