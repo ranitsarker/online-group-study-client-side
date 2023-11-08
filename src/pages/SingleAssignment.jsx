83c9ef29b71d2a0417f9513ad09b9373c9b6dc58
ranitsarker <rntprince@gmail.com>
2023-11-08 16:57:17 +0600

    added credenticals include in fetching data to verify endpoint with jwt verifyToken

--- src/pages/SingleAssignment.jsx
+++ src/pages/SingleAssignment.jsx
@@ -8,7 +8,9 @@ const SingleAssignment = () => {
     const [eachAssignment, setEachAssignment] = useState(null);
 
     useEffect(() => {
-        fetch(`http://localhost:5000/assignment/${id}`)
+        fetch(`http://localhost:5000/assignment/${id}`, {
+            credentials: "include"
+        })
             .then((res) => res.json())
             .then((data) => {
                 setEachAssignment(data);
